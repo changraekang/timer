53a8312c788a0343f9a8abc2a8f4220e1e685442
Changraekang <storkcr@gmail.com>
2022-07-28 18:24:48 +0900

    class형 컴포넌트 타이머 설정

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import Clock from './Clock';
+import Timer from './Timer';
+import Example from './Example';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <hi>타이머</hi>
+     <Clock />
+     <Timer />
+     <Example />
     </div>
   );
 }
